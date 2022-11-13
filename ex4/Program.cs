52945ff7d31a5913980d6fcd534f70897b3aca5c
P.Kurakin <Kurakin93@gmail.com>
2022-11-13 22:08:48 +0300

    Ex4 well done

---
+++ ex4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Ввести три числа");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (max < b) max = b;
+if (max < c) max = c;
+Console.Write("Max = ");
+Console.WriteLine(max);
+
