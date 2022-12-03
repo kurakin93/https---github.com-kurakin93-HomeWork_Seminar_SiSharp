718fb7b7001142aa9119b04df4d800af5c7e2bf2
P.Kurakin <Kurakin93@gmail.com>
2022-12-03 19:54:33 +0300

    Решил 43 и 41 задачу

---
+++ S6/Ex1/Program.cs
@@ -0,0 +1,30 @@
+﻿//              Задача 41: Пользователь вводит с клавиатуры M чисел. 
+//              Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Задача 41: Пользователь вводит с клавиатуры M чисел.");
+Console.WriteLine("Посчитайте, сколько чисел больше 0 ввёл пользователь.");
+Console.WriteLine();
+Console.WriteLine("Введите числа с клавиатуры через пробел! ");
+string numbers = Console.ReadLine();
+int[] arrayNumb = numbers.Split(' ').Select(int.Parse).ToArray();
+
+
+Console.WriteLine($"Больше 0: {CountGreaterThan0(arrayNumb)}");
+
+
+int CountGreaterThan0(int[] num)
+{
+    int count = 0;
+    for(int i = 0; i < arrayNumb.Length; i++)
+    {
+        if(arrayNumb[i] >= 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
---
+++ S6/Ex2/Program.cs
@@ -0,0 +1,38 @@
+﻿// // Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+// // y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, 
+// k1 = 5, 
+// b2 = 4, 
+// k2 = 9 -> 
+// (-0,5; -0,5)
+
+Console.WriteLine("Введите значение b1");
+double numB1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение k1");
+double numK1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение b2");
+double numB2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение k1");
+double numK2 = Convert.ToInt32(Console.ReadLine());
+doubleersectionPodouble(numB1, numK1, numB2, numK2);
+
+void doubleersectionPodouble(double numB1, double numK1, double numB2, double numK2)
+{
+    if(numK1 == numK2)
+    {
+        Console.WriteLine("Прямые параллельны.");
+    }
+    else if(numK1 == -(1/numK2))
+    {
+        double y = (numK2*numB1-numK1*numB2)/(numK2-numK1);
+        double x = (numB1-numB2)/(numK2-numK1);
+        Console.WriteLine($"Прямые перпендикулярны. Точка пересечения ({y},{x})");
+    }
+    else
+    {
+    double y = (numK2*numB1-numK1*numB2)/(numK2-numK1);
+    double x = (numB1-numB2)/(numK2-numK1);
+    Console.WriteLine($"Точка пересечения ({y},{x})");
+    }
+}
