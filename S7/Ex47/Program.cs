a040f4b97dd14ddb9b66c4543fdcbe3a3d44ca42
P.Kurakin <Kurakin93@gmail.com>
2022-12-04 14:01:13 +0300

    47, 50, 52 задачи добавлены

---
+++ S7/Ex47/Program.cs
@@ -0,0 +1,33 @@
+﻿// 1. Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+
+Console.WriteLine("Задайте двумерный массив размером m*n. ");
+Console.WriteLine("Input number lines: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number columns: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+double[,] arrayNumber = new double[numberM, numberN];
+var random = new Random();
+
+PrintArray(numberM, numberN);
+
+void PrintArray(int numberM, int numberN)
+{
+    double[,] arrNum = new double[numberM, numberN];
+    for(int i = 0; i < arrNum.GetLength(0); i++)
+    {
+        for(int j = 0; j < arrNum.GetLength(1); j++)
+        {
+            arrNum[i, j] = random.Next(-5, 5);
+            Console.Write($" {1/arrNum[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+    
+}
---
+++ S7/Ex50/Program.cs
@@ -0,0 +1,24 @@
+﻿// 2. Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+
+Console.WriteLine("Введи позицию элемента: ");
+int numPositionI = Convert.ToInt32(Console.ReadLine());
+int numPositionJ = Convert.ToInt32(Console.ReadLine());
+int[,] arrayTable = {{1, 2, -2, -4}, {-2, 3, 0, 23}, {1, 2, 3, 4}};
+FindNumber(arrayTable);
+
+void FindNumber(int[,] arrNum)
+{
+    if(numPositionI > arrayTable.GetLength(0) | numPositionJ > arrayTable.GetLength(1))
+    {
+        Console.WriteLine($"Такого числа нет! ");
+    }
+    else Console.WriteLine($" {arrayTable[numPositionI, numPositionJ]} ");
+}
